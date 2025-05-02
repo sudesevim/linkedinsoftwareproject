e8d682a5dc0f2b2eb812ba71a5e18d73e684f2af
rabialafci <rabialafci123@gmail.com>
2025-05-02 18:22:48 +0300

    arızaaa-33dk

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js"; 
@@ -9,8 +11,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+if (process.env.NODE_ENV !== "production") {
+	app.use(
+		cors({
+			origin: "http://localhost:5173",
+			credentials: true,
+		})
+	);
+}
 
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
