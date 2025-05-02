30ca7223e9081ce868e4db2ee540ae8e37d47768
gozdeyildz <158503297+gozdeyildz@users.noreply.github.com>
2025-05-02 20:53:50 +0300

    add mail controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,16 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 
 export const signup = async (req, res) => {
     try {
 
         const { name, username, email, password } = req.body;
+        if (!name || !username || !email || !password) {
+			return res.status(400).json({ message: "All fields are required" });
+		}
         const existingEmail = await User.findOne({ email });
         if (existingEmail) {
 			return res.status(400).json({ message: "Email already exists" });
@@ -39,7 +43,15 @@ export const signup = async (req, res) => {
 		})
 
         res.status(201).json({ message: "User registered successfully" });
+        const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
 
+        try{
+            await sendWelcomeEmail(user.email,user.name,profileUrl)
+            
+        }catch(emailError){
+            console.error("Error sending welcome Email", emailError);
+
+        }
     } catch (error) {
         console.log("Error in signup: ", error.message);
 		res.status(500).json({ message: "Internal server error" });
---
+++ backend/emails/emailHandlers.js
@@ -0,0 +1,22 @@
+import { mailtrapClient, sender } from "../lib/mailtrap.js";
+
+export const sendWelcomeEmail = async (email, name, profileUrl) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Welcome to UnLinked",
+			html: createWelcomeEmailTemplate(name, profileUrl),
+			category: "welcome",
+		});
+
+		console.log("Welcome Email sent succesffully", response);
+	} catch (error) {
+		throw error;
+	}
+};
+
+
+
