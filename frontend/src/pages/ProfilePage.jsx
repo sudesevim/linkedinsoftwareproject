11b5aaa2811401b74985b0a7dde9ef5d81459aad
Sude Sevim <sudesevim0@gmail.com>
2025-05-11 19:36:48 +0300

    update my profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -93,11 +93,15 @@ const ProfilePage = () => {
 
 		return (
 			<div className='max-w-4xl mx-auto p-4'>
-				<ProfileHeader userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
-				<AboutSection userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
-				<ExperienceSection userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
-				<EducationSection userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
-				<SkillsSection userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
+				<div className="row justify-content-center">
+					<div className="col-md-6">
+						<ProfileHeader userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
+						<AboutSection userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
+						<ExperienceSection userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
+						<EducationSection userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
+						<SkillsSection userData={userData} isOwnProfile={isOwnProfile} onSave={updateProfile} />
+					</div>
+				</div>
 			</div>
 		);
 	}
